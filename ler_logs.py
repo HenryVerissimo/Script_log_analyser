37b56a7c2396fd007675a9f9caa6d35b11600a8e
Henrique Verissimo <henriqueverissimocontato@gmail.com>
2026-03-06 16:39:26 -0300

    Create script file

---
+++ ler_logs.py
@@ -0,0 +1,28 @@
+import os
+
+from typing import List
+
+FILE_NAME = "access.log"
+observed_ips: List[str] = []
+
+def get_absolute_file_path(file_name: str) -> str:
+
+    if isinstance(file_name, str) and os.path.exists(file_name) and os.path.isfile(file_name):
+        return os.path.abspath(file_name)
+    
+    else:
+        raise Exception("Arquivo inválido! Forneça o nome de um arquivo de log válido!")
+
+
+file_path: str = get_absolute_file_path(FILE_NAME)
+
+with open(file_path, "r") as file:
+    for line in file:
+        if "404" in line.split(" "):
+            observed_ips.append(line.split(" ")[0])
+
+
+print("IPs que fixeram requições suspeitas:")
+
+for ip in observed_ips:
+    print(ip)
